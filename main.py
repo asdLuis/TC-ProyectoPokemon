79614d2a9c804935abae2c57b9d324fc22ed54cb
zLuis <77698232+asdLuis@users.noreply.github.com>
2022-08-24 17:02:35 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,151 @@
+import random
+from time import sleep
+
+Pokedex = []
+xp = 0
+fruit = 5
+energy = 100
+
+def selectPokemon():
+    print("Welcome to the world of Pokemon, as a new trainer you can only choose one pokemon. But first, you must choose your name:")
+    name = input("Enter your name: ")
+    print("Hello " + name + ", you must now choose your pokemon! Take a look at the list below and choose wisely as this will be your only pokemon for now.")
+    print("1. Charmander")
+    print("2. Squirtle")
+    print("3. Bulbasaur")
+    pokemon = int(input("Select your pokemon: "))
+    if pokemon == 1:
+        print("You have selected Charmander, great choice!") #■
+        print("Charmander is a fire type pokemon, it is weak against water and strong against grass. An interesting fact about charmander is that")
+        print("it can evolve into 3 different pokemon, Charmeleon, Charizard and Mega Charizard X.")
+        print("Your charmander is currently: Level 1, it has 20 hp and 10 Attack.")
+        Pokedex.append("Charmander")
+    elif pokemon == 2:
+        print("You have selected Squirtle, interesting choice!")
+        print("Squirtle is a water type pokemon, it is weak against grass and strong against fire. An interesting fact about squirtle is that")
+        print('it can evolve into 3 different pokemon, Wartortle, Blastoise and Mega Blastoise.')
+        print('Your squirtle is currently: Level 1, it has 20 hp and 10 Attack.')
+        Pokedex.append("Squirtle")
+    elif pokemon == 3:
+        print("You have selected Bulbasaur, good choice!")
+        print("Bulbasaur is a grass type pokemon, it is weak against fire and strong against water. An interesting fact about bulbasaur is that")
+        print("it can evolve into 3 different pokemon, Ivysaur, Venusaur and Mega Venusaur.")
+        print('Your bulbasaur is currently: Level 1, it has 20 hp and 10 Attack.')
+        Pokedex.append("Bulbasaur")
+    else:
+        print("Invalid option, please try again")
+
+def getCapturedPokemon(pokemonCaptured):
+    global xp
+    print("You have captured a " + pokemonCaptured + " and gained 100 xp!")
+    Pokedex.append(pokemonCaptured)
+    print("This is what your Pokedex looks like: ", Pokedex)
+    xp += 100
+
+def getRandomPokemon():
+    randomPokemon = random.randint(1, 9)
+    if randomPokemon == 1:
+        pokemonCaptured = "Eevee"
+        getCapturedPokemon(pokemonCaptured)
+    elif randomPokemon == 2:
+        pokemonCaptured = "Pikachu"
+        getCapturedPokemon(pokemonCaptured)
+    elif randomPokemon == 3:
+        pokemonCaptured = "Onix"
+        getCapturedPokemon(pokemonCaptured)
+    elif randomPokemon == 4:
+        pokemonCaptured = "Meowth"
+        getCapturedPokemon(pokemonCaptured)
+    elif randomPokemon == 5:
+        pokemonCaptured = "Pysduck"
+        getCapturedPokemon(pokemonCaptured)
+    elif randomPokemon == 6:
+        pokemonCaptured = "Cubeone"
+        getCapturedPokemon(pokemonCaptured)
+    elif randomPokemon == 7:
+        pokemonCaptured = "Togepi"
+        getCapturedPokemon(pokemonCaptured)
+    elif randomPokemon == 8:
+        pokemonCaptured = "Turtwig"
+        getCapturedPokemon(pokemonCaptured)
+    elif randomPokemon == 9:
+        pokemonCaptured = "Pigeon"
+        getCapturedPokemon(pokemonCaptured)
+
+def eatFruit():
+    global fruit
+    global energy
+    if fruit > 0:
+        print("You are eating fruit...")
+        sleep(2)
+        fruit -= 1
+        energy += 10
+        explore()
+    else:
+        print("You have no fruit")
+        explore()
+
+def actions():
+        global fruit
+        global energy
+        global xp
+        print(" 1. Fight\n 2. Run\n 3. Eat fruit\n 4. Rest\n 5. Check Energy\n 6. Check Fruit\n 7. Check XP")
+        option = int(input("Select an option: "))
+        if option == 1:
+            combat() 
+        elif option == 2:
+            print("You are running")
+            explore()
+        elif option == 3:
+            eatFruit()
+        elif option == 4:
+            print("You are resting...")
+            sleep(15)
+            energy += 10
+            explore()
+        elif option == 5:
+            print("Your energy is: ", energy)
+            explore()
+        elif option == 6:
+            print("You have", fruit, "fruit")
+            explore()
+        elif option == 7:
+            print("Your XP is: ", xp)
+            explore()
+        else:
+            print("Invalid option")
+            explore()   
+
+def combat():
+    global energy
+    print("You have engaged in combat with a wild pokemon!")
+    winningOdds = random.randint(1, 2)
+    if winningOdds == 1:
+        getRandomPokemon()
+        explore()
+    elif winningOdds == 2:
+        print("You have lost the battle, run away!")
+        energy -= 10
+        explore()
+
+def explore():
+    global energy
+    global fruit
+    if energy > 0:
+        print("You are exploring the wild")
+        print("You have found a wild pokemon")
+        actions()
+    else:
+        print("You have no energy left, you must rest or eat a fruit to regain energy. You will be able to explore again in 15 seconds if you sleep.")
+        option = int(input("1. Rest\n2. Eat a fruit\nSelect an option: "))
+        if option == 1:
+            sleep(15)
+            energy = 20
+            print('You have regained 20 energy!')
+            explore()
+        elif option == 2:
+            eatFruit()
+        else:
+            print("Invalid option")
+selectPokemon()
+explore()
